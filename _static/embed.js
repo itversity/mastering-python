90bdf24a572218f4e05235036f16f86bcf8a8195
itversity <dgadiraju@itversity.com>
2020-12-27 13:55:50 +0530

    Cleaned up javascripts

--- _static/embed.js
+++ _static/embed.js
@@ -3,7 +3,7 @@ window.DiscourseEmbed = { discourseUrl: 'http://discuss.itversity.com/t/git-hub-
 
 (function() {
   var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;
-  d.src = 'https://meta.discourse.org/javascripts/embed.js';
+  d.src = window.DiscourseEmbed.discourseUrl + 'javascripts/embed.js';
   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
 })();
 
