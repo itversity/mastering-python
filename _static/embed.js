073e5b596d38b803c423fd4d5cc2c9e95fe3842a
itversity <dgadiraju@itversity.com>
2020-12-27 12:06:44 +0530

    Updated static to embed discuss topics

--- _static/embed.js
+++ _static/embed.js
@@ -1,12 +1,8 @@
-<div id='discourse-comments'></div>
+window.DiscourseEmbed = { discourseUrl: 'http://discuss.itversity.com/',
+                   topicId: 8};
 
-<script type="text/javascript">
-  window.DiscourseEmbed = { discourseUrl: 'http://discuss.itversity.com/',
-                     topicId: 8};
-
-  (function() {
-    var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;
-    d.src = window.DiscourseEmbed.discourseUrl + 'javascripts/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
-  })();
-</script>
+(function() {
+  var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;
+  d.src = 'javascripts/embed.js';
+  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
+})();
