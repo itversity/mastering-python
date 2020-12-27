4b6b86db0cf635ea62608cc4d704cf0853383cee
itversity <dgadiraju@itversity.com>
2020-12-27 11:41:14 +0530

    Update documentation

---
+++ _static/embed.js
@@ -0,0 +1,12 @@
+<div id='discourse-comments'></div>
+
+<script type="text/javascript">
+  window.DiscourseEmbed = { discourseUrl: 'http://discuss.itversit.com/',
+                     topicId: 8};
+
+  (function() {
+    var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;
+    d.src = window.DiscourseEmbed.discourseUrl + 'javascripts/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
+  })();
+</script>
