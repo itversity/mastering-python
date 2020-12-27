ed3187503b70f368e82fb8421b2cde21a811e831
itversity <dgadiraju@itversity.com>
2020-12-27 12:28:33 +0530

    Update documentation

--- _static/embed.js
+++ _static/embed.js
@@ -1,8 +1,9 @@
-window.DiscourseEmbed = { discourseUrl: 'http://discuss.itversity.com/',
-                   topicId: 8};
+window.DiscourseEmbed = { discourseUrl: 'http://discuss.itversity.com/t/git-hub-link-request-for-pyspark-related-module/',
+                   topicId: 23884};
 
 (function() {
   var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;
   d.src = 'https://meta.discourse.org/javascripts/embed.js';
   (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
 })();
+
